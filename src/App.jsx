f6428ab40652ea6302ec2f3f669f3d6dfa898b61
Emiliano Gusberti <emggw@hotmail.com>
2023-06-17 12:49:33 -0300

    Add app.css and initial fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-function App() {
+import { useState, useEffect } from "react";
+import "./App.css";
+
+export default function App() {
+  const [charList, setCharList] = useState([]);
+
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/character")
+      .then((response) => response.json())
+      .then(({ results: characters }) => {
+        // Recorrer la lista de personajes y mostrar los datos en pantalla
+        setCharList(characters);
+      })
+      .catch((error) => {
+        console.log("Error:", error);
+      });
+  }, []);
 
   return (
     <>
+      <div>{charList ? charList.map((char) => <h4 key={char.id} id={char.id}>{char.name}</h4>) : <h1>Nada</h1>}</div>
     </>
-  )
+  );
 }
-
-export default App
